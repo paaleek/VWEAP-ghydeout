d25d23f19904a9351e9cdcf6d5e2a37ae561fa94
PGalcik <pavol.galcik@frequentis.com>
2025-02-01 16:34:07 +0100

    Refactor email sending

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,60 +1,18 @@
 import { RequestHandler } from "express";
-import nodemailer from "nodemailer";
-import path from "path";
 
 import { CreateUser } from "#/@types/user";
 import User from "#/models/user";
-import EmailVerificationToken from "#/models/emailVerificationToken";
-import { config } from "#/utils/variables";
 import { generateToken } from "#/utils/helper";
-import { generateTemplate } from "#/mail/template";
+import { sendVerificationMail } from "#/mail/mail";
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
   const user = await User.create({ email, password, name });
 
-  //send verifycation email
-  var transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: config.MAILTRAP_USER,
-      pass: config.MAILTRAP_PASSWORD,
-    },
-  });
-
+  //send verification email
   const token = generateToken(10);
-  const verificationToken = await EmailVerificationToken.create({
-    owner: user._id,
-    token: token,
-  });
 
-  const welcomeMessage = `Ahoj ${name}, vitaj v BZZVÁHY. Klikni na tlačidlo a over svoj email.`;
+  sendVerificationMail(token, { name, email, userId: user._id.toString() }); //here can be wait for sending email
 
-  transport.sendMail({
-    to: user.email,
-    from: config.SERVER_AUTH_EMAIL,
-    html: generateTemplate({
-      title: "Vitajte v BZZ VÁHY.",
-      message: welcomeMessage,
-      logo: "cid:logo",
-      banner: "cid:welcome",
-      link: "#",
-      btnTitle: token,
-    }),
-    attachments: [
-      {
-        filename: "logo.png",
-        path: path.join(__dirname, "../mail/assets/logo.png"),
-        cid: "logo",
-      },
-      {
-        filename: "password_reset.png",
-        path: path.join(__dirname, "../mail/assets/password_reset.png"),
-        cid: "welcome",
-      },
-    ],
-  });
-
-  res.status(201).json({ user });
+  res.status(201).json({ user: { id: user._id, name, email } });
 };
---
+++ src/mail/mail.ts
@@ -0,0 +1,62 @@
+import nodemailer from "nodemailer";
+import path from "path";
+
+import EmailVerificationToken from "#/models/emailVerificationToken";
+import { config } from "#/utils/variables";
+import { generateTemplate } from "#/mail/template";
+
+const getMailTransporter = () => {
+  //send verifycation email
+  return nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: config.MAILTRAP_USER,
+      pass: config.MAILTRAP_PASSWORD,
+    },
+  });
+};
+
+interface Profile {
+  name: string;
+  email: string;
+  userId: string;
+}
+
+export const sendVerificationMail = async (token: string, profile: Profile) => {
+  const transport = getMailTransporter();
+
+  const { name, email, userId } = profile;
+
+  const verificationToken = await EmailVerificationToken.create({
+    owner: userId,
+    token: token,
+  });
+
+  const welcomeMessage = `Ahoj ${name}, vitaj v BZZVÁHY. Klikni na tlačidlo a over svoj email.`;
+
+  transport.sendMail({
+    to: email,
+    from: config.SERVER_AUTH_EMAIL,
+    html: generateTemplate({
+      title: "Vitajte v BZZVÁHY.",
+      message: welcomeMessage,
+      logo: "cid:logo",
+      banner: "cid:welcome",
+      link: "#",
+      btnTitle: token,
+    }),
+    attachments: [
+      {
+        filename: "logo.png",
+        path: path.join(__dirname, "../mail/assets/logo.png"),
+        cid: "logo",
+      },
+      {
+        filename: "password_reset.png",
+        path: path.join(__dirname, "../mail/assets/password_reset.png"),
+        cid: "welcome",
+      },
+    ],
+  });
+};
